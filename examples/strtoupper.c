d177712a2dc0a18beacf9efb274e219cbc313243
Constantin Wiemer <constantin.wiemer@gmx.de>
2017-09-28 18:47:39 +0200

    Changed the example program strtoupper a bit

--- examples/strtoupper.c
+++ examples/strtoupper.c
@@ -1,10 +1,14 @@
-void start()
+// The string needs to be defined as global *array* (not pointer), otherwise GCC
+// will treat it as a constant and put it into the .rdata segment, which gets
+// memory-mapped read-only by our loader and the string can therefore not be changed.
+static char s[] = "hello world";
+
+int start()
 {
-    char *s = "hello world";
     char *p;
     for (p = s; *p != 0; p++) {
         if (*p >= 97 && *p <= 122)
             *p -= 32;
     }
-    return;
+    return 0;
 }
