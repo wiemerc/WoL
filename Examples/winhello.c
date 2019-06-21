1f686a84932d3451ee65c24705642a2c04b5c045
Constantin Wiemer <constantin.wiemer@gmx.de>
2019-06-21 21:05:17 +0200

    Custom startup code is no longer used

--- Examples/winhello.c
+++ Examples/winhello.c
@@ -1,16 +1,16 @@
-/*
- * winhello.c - my first attempt at Windows system programming
- */
+//
+// winhello.c - my first attempt at Windows system programming
+//
 
 
 #include <windows.h>
 
 
-int cwmain()
+// We don't use a startup code nor the standard library, hence no main() function
+void start()
 {
     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD  nOut;
 
     WriteFile(hStdOut, "Hello, Windows\r\n", 16, &nOut, NULL);
-    return 0;
 }
